7fe90ece11f70cbb51c9c38c320d49dfdf017873
Stupercell <alexanderjschafer@gmail.com>
2026-05-15 14:42:16 -0400

    Updates

--- server/assets/_tanstack-start-manifest_v-C4PzH7Dl.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Alex/Downloads/DAT Rework/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BeOA-vcH.js"] }, "/": { filePath: "C:/Users/Alex/Downloads/DAT Rework/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Cp_qwsy-.js"] } }, clientEntry: "/assets/index-BeOA-vcH.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-D5EH8KF2.js";
-import "node:events";
-export {
-  w as default
-};
